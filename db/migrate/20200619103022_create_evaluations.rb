66ec33e8b096148d209b32d567abdd0ca016f2e6
pixar2390 <pixar_duffy@icloud.com>
2020-06-19 23:28:11 +0900

    Update 20200619103022_create_evaluations.rb

--- db/migrate/20200619103022_create_evaluations.rb
+++ db/migrate/20200619103022_create_evaluations.rb
@@ -1,7 +1,10 @@
 class CreateEvaluations < ActiveRecord::Migration[5.0]
   def change
     create_table :evaluations do |t|
-
+      t.integer :evaluation, default: 0, null: false    #enumメソッドをモデルクラス内に記述のこと
+      t.stirng :review
+      t.references :item, null: false ,foreign_key: true
+      t.references :user, null: false ,foreign_key: true
       t.timestamps
     end
   end
