c0a507e7efbf590b5c356e4363fd1861a718a024
honshin4n1 <million_films.sh@icloud.com>
2020-06-19 21:12:58 +0900

    create sending_destination_model
    sending_destinationsテーブル作成

--- db/migrate/20200619112932_create_preparation_days.rb
+++ db/migrate/20200619112932_create_preparation_days.rb
@@ -1,8 +1,8 @@
 class CreatePreparationDays < ActiveRecord::Migration[5.0]
   def change
     create_table :preparation_days do |t|
-      t.string :preparation_day, null: false
-      t.references :item, null: false, foreign_key: true
+      t.string     :preparation_day, null: false
+      t.references :item,            null: false, foreign_key: true
       t.timestamps
     end
   end
--- db/migrate/20200619113051_create_postage_payers.rb
+++ db/migrate/20200619113051_create_postage_payers.rb
@@ -1,8 +1,8 @@
 class CreatePostagePayers < ActiveRecord::Migration[5.0]
   def change
     create_table :postage_payers do |t|
-      t.string :postage_payer, null: false
-      t.references :item, null: false, foreign_key: true
+      t.string     :postage_payer, null: false
+      t.references :item,          null: false, foreign_key: true
       t.timestamps
     end
   end
