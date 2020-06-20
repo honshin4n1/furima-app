baeb0127dd4e9f06f13fe1431c1e57ec43219271
pixar2390 <pixar_duffy@icloud.com>
2020-06-20 18:29:50 +0900

    Update 20200619103121_create_favorites.rb

--- db/migrate/20200619103121_create_favorites.rb
+++ db/migrate/20200619103121_create_favorites.rb
@@ -1,8 +1,8 @@
 class CreateFavorites < ActiveRecord::Migration[5.0]
   def change
     create_table :favorites do |t|
-      t.references :item, null: false ,foreign_key: true
-      t.references :user, null: false ,foreign_key: true
+      # t.references :item, null: false ,foreign_key: true
+      # t.references :user, null: false ,foreign_key: true
       t.timestamps
     end
   end
