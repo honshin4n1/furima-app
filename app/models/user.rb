7906bbf9e7c53a7112ff197b1bd89df8a3cb4982
DXnabe02 <watanabe02masahide02@gmail.com>
2020-06-20 14:52:11 +0900

    Introduction of devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,13 @@
 class User < ApplicationRecord
-  has_many: comments, dependent: destroy
-  has_many: favorites
-  has_many: evaluations
-  has_one: profile, dependent: destroy
-  has_one: sending_destination, dependent: destroy
-  has_one: credit_card, dependent: destroy
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  
+  has_many :comments, dependent: :destroy
+  has_many :favorites
+  has_many :evaluations
+  has_one :profile, dependent: :destroy
+  has_one :sending_destination, dependent: :destroy
+  has_one :credit_card, dependent: :destroy
 end
