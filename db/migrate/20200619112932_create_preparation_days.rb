eb1b56214fdf61d00b71f347b61e5776d156e7a2
pixar2390 <pixar_duffy@icloud.com>
2020-06-20 18:29:44 +0900

    Update 20200619112932_create_preparation_days.rb

--- db/migrate/20200619112932_create_preparation_days.rb
+++ db/migrate/20200619112932_create_preparation_days.rb
@@ -2,7 +2,7 @@ class CreatePreparationDays < ActiveRecord::Migration[5.0]
   def change
     create_table :preparation_days do |t|
       t.string     :preparation_day, null: false
-      t.references :item,            null: false, foreign_key: true
+      # t.references :item,            null: false, foreign_key: true
       t.timestamps
     end
   end
