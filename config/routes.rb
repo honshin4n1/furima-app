5fa1a6a9bae735a054b5fa2d7ac144817577406b
DXnabe02 <watanabe02masahide02@gmail.com>
2020-06-25 18:26:38 +0900

    edit watanabe

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
   resources :items, only: [:index, :new ,:create]
-
+  resources :users, only: :show
+  resources :logouts, only: :index
+  resources :cards, only: [:new, :create]
 end
