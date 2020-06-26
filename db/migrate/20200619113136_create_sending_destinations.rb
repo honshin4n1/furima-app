aebba9be0408fb78d713fb5d2e07ad6e1aa6490f
pixar2390 <pixar_duffy@icloud.com>
2020-06-26 15:10:51 +0900

    Update 20200619113136_create_sending_destinations.rb

--- db/migrate/20200619113136_create_sending_destinations.rb
+++ db/migrate/20200619113136_create_sending_destinations.rb
@@ -5,12 +5,12 @@ class CreateSendingDestinations < ActiveRecord::Migration[5.0]
       t.string     :family_name,      null: false
       t.string     :first_name_kana,  null: false
       t.string     :family_name_kana, null: false
-      t.integer    :post_code,        null: false
-      t.integer    :prefecture,       null: false
+      t.string     :post_code,        null: false
+      t.string     :prefecture,       null: false
       t.string     :city,             null: false
       t.string     :house_number,     null: false
       t.string     :building_name
-      t.integer    :phone_number,     unique: true
+      t.string     :phone_number,     unique: true
       # t.references :user,             null: false, foreign_key: true
       t.timestamps
     end
