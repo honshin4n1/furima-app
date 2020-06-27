7a6cf3d9b599b95742efe4dd66e1af99b154d8cc
honshin4n1 <million_films.sh@icloud.com>
2020-06-27 21:07:57 +0900

    add validates models user profile

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,5 +1,8 @@
 class Profile < ApplicationRecord
-  # belongs_to :user
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :prefecture
+  belongs_to :user, optional: true
+  validates :first_name, :family_name, 
+            :first_name_kana, :family_name_kana, 
+            :year, :month, :day, presence: true
+  # extend ActiveHash::Associations::ActiveRecordExtensions
+  # belongs_to_active_hash :prefecture
 end
