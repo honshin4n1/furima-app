71630140128b66bc61028332c260fdff01ba905c
pixar2390 <pixar_duffy@icloud.com>
2020-06-27 19:59:05 +0900

    Update 20200607100536_create_items.rb

--- db/migrate/20200607100536_create_items.rb
+++ db/migrate/20200607100536_create_items.rb
@@ -4,12 +4,12 @@ class CreateItems < ActiveRecord::Migration[5.0]
       t.string      :name,              null: false
       t.text        :introduction,      null: false
       t.integer     :price,             null: false
-      t.integer     :size,              null: false
-      t.integer     :condition,         null: false
+      t.string      :size,              null: false
+      t.string      :condition,         null: false
       t.boolean     :deal_state,        null: false, default: 0   # 0 => が売り出し中、１=> 売り切れ
       t.integer     :buyer
       t.references  :user,             null: false, foreign_key: true
-      t.references  :brand,            null: false, foreign_key: true
+      t.references  :brand,            foreign_key: true
       t.references  :category,         null: false, foreign_key: true
       t.timestamps
     end
