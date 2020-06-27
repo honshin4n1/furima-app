e7c0f1e3580e26ed87bb946facaac8264fba5090
pixar2390 <pixar_duffy@icloud.com>
2020-06-27 20:10:01 +0900

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-table_names = %w(users brands categories items)
+table_names = %w(users brands categories items item_images)
 table_names.each do |table_name|
   path = Rails.root.join("db/seeds", Rails.env, table_name + ".rb")
   if File.exist?(path)
