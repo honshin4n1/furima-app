948da18edec1c614eb7a7b2ffbb1f9617c9ce2bb
pixar2390 <pixar_duffy@icloud.com>
2020-06-27 19:25:50 +0900

    Update items.rb

---
+++ db/seeds/development/items.rb
@@ -0,0 +1,18 @@
+# create_table :items do |t|
+#   t.string      :name,              null: false
+#   t.text        :introduction,      null: false
+#   t.integer     :price,             null: false
+#   t.integer     :size,              null: false
+#   t.integer     :condition,         null: false, default: 0
+#   t.boolean     :deal_state,        null: false
+#   t.integer     :buyer
+#   t.references  :user,             null: false, foreign_key: true
+#   t.references  :brand,            null: false, foreign_key: true
+#   t.references  :category,         null: false, foreign_key: true
+#   t.timestamps
+
+names = %w()
+introductions = %w()
+prices = %w()
+sizes =
+conditons =
