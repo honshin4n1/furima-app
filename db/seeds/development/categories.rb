a551c0b66d6a3b87e3a5411fbeabaab4675a3be3
pixar2390 <pixar_duffy@icloud.com>
2020-06-27 16:38:18 +0900

    Update categories.rb

--- db/seeds/development/categories.rb
+++ db/seeds/development/categories.rb
@@ -3,7 +3,8 @@ level_1 = %w(レディース
             メンズ
             ベビー・キッズ
             インテリア・住まい・小物
-            本・音楽・ゲーム)
+            本・音楽・ゲーム
+            おもちゃ・ホビー・グッズ)
 level_1.each do |cat|
   Category.create(name: cat)
 end
@@ -68,4 +69,51 @@ level_2 = %w(ベビー服（女の子用）〜95cm
 # データを生成
 level_2.each do |cat|
   Category.find_by(name:"ベビー・キッズ").children.create(name: cat)
+end
+# ーーーーーーーーーーーーーーーーーーーーー
+# <インテリア・住まい・小物>
+level_2 = %w(キッチン/食器
+            ベット/マットレス
+            ソファ/ソファベット
+            椅子/チェア
+            机/テーブル
+            収納家具
+            ラグ/カーペット/マット
+            カーテン/ブラインド
+            ライト/照明
+            寝具
+            インテリア小物
+            季節/年中行事)
+# データを生成
+level_2.each do |cat|
+  Category.find_by(name:"インテリア・住まい・小物").children.create(name: cat)
+end
+# ーーーーーーーーーーーーーーーーーーーーー
+# <本・音楽・ゲーム>
+level_2 = %w(本
+            漫画
+            雑誌
+            CD
+            DVD/ブルーレイ
+            レコード
+            テレビゲーム)
+# データを生成
+level_2.each do |cat|
+  Category.find_by(name:"本・音楽・ゲーム").children.create(name: cat)
+end
+# ーーーーーーーーーーーーーーーーーーーーー
+# <おもちゃ・ホビー・グッズ>
+level_2 = %w(おもちゃ
+            タレントグッズ
+            コミック/アニメグッズ
+            トレーディングカード
+            フィギア
+            楽器/器材
+            コレクション
+            ミリタリー
+            美術品
+            アート用品)
+# データを生成
+level_2.each do |cat|
+  Category.find_by(name:"おもちゃ・ホビー・グッズ").children.create(name: cat)
 end
