9b33e47c36c0db71ea4e6767ae45285ce12ae252
pixar2390 <pixar_duffy@icloud.com>
2020-06-28 23:12:44 +0900

    Update items.rb

--- db/seeds/development/items.rb
+++ db/seeds/development/items.rb
@@ -1,16 +1,3 @@
-# create_table :items do |t|
-#   t.string      :name,              null: false
-#   t.text        :introduction,      null: false
-#   t.integer     :price,             null: false
-#   t.integer     :size,              null: false
-#   t.integer     :condition,         null: false
-#   t.boolean     :deal_state,        null: false
-#   t.integer     :buyer
-#   t.references  :user,             null: false, foreign_key: true
-#   t.references  :brand,            null: false, foreign_key: true
-#   t.references  :category,         null: false, foreign_key: true
-#   t.timestamps
-
 names = %w(ハトムギ化粧水 ipohne10 Echo_Dot 長財布 ワンピース)
 introductions = %w(めっちゃいい 感動した 今が買い時 経年劣化により味が出てます 一部滲みがございます)
 prices = %w(1000 98000 32000 40000 4000)
