a58742ad19d3b74c6090497b3366050490151108
pixar2390 <pixar_duffy@icloud.com>
2020-06-29 17:48:18 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -112,9 +112,9 @@ ActiveRecord::Schema.define(version: 20200619113051) do
     t.string   "first_name_kana",  null: false
     t.string   "family_name_kana", null: false
     t.string   "introduction"
-    t.integer  "year"
-    t.integer  "month"
-    t.integer  "day"
+    t.integer  "year",             null: false
+    t.integer  "month",            null: false
+    t.integer  "day",              null: false
     t.integer  "user_id",          null: false
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
@@ -141,12 +141,12 @@ ActiveRecord::Schema.define(version: 20200619113051) do
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
+    t.string   "nickname",                            null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "nickname",                            null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
