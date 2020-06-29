df20154741e035bfd0f47be7c214b62d7593dd0b
honshin4n1 <million_films.sh@icloud.com>
2020-06-29 18:06:35 +0900

    add routing devise_scope

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations' }
+  devise_scope :user do
+    get 'sending_destinations', to: 'users/registrations#new_sending_destination'
+    post 'sending_destinations', to: 'users/registrations#create_sending_destination'
+  end
   
   root to: 'items#index'
   resources :items, only: [:index, :new ,:create, :show] do
