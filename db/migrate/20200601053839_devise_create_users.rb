5c2a8d2a335f1c66a0543b63547ba1697d2d94fb
pixar2390 <pixar_duffy@icloud.com>
2020-06-29 14:28:27 +0900

    Update 20200601053839_devise_create_users.rb

--- db/migrate/20200601053839_devise_create_users.rb
+++ db/migrate/20200601053839_devise_create_users.rb
@@ -6,7 +6,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-
+      t.string :nickname, null: false
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
@@ -32,9 +32,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-
       t.timestamps null: false
-      t.string :nickname, null: false
     end
 
     add_index :users, :email,                unique: true
