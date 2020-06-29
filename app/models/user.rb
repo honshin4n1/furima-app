49deeab11bff3661837f57fc003d18465cf44a54
honshin4n1 <million_films.sh@icloud.com>
2020-06-29 18:07:26 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, password_length: 7..128
-         validates :nickname, presence: true
+  validates :nickname, presence: true, uniqueness: true
 
   # has_many :comments, dependent: :destroy
   # has_many :favorites
   # has_many :evaluations
-  has_one :profile
+  has_one :profile, dependent: :destroy
   accepts_nested_attributes_for :profile
-  # has_one :sending_destination, dependent: :destroy
+  has_one :sending_destination, dependent: :destroy
   # has_one :credit_card, dependent: :destroy
 end
