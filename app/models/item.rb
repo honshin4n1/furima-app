c646d7d85f66e23c3e50f7967a51276a6b51ce64
pixar2390 <pixar_duffy@icloud.com>
2020-06-29 12:44:49 +0900

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   has_many :comments, dependent: :destroy
   has_many :item_images, dependent: :destroy
   has_many :evaluations
@@ -6,6 +7,7 @@ class Item < ApplicationRecord
   belongs_to :category
   belongs_to :brand
   belongs_to :user
-  has_one :postage_payer
-  has_one :preparation_day
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :preparation_day
+  belongs_to_active_hash :postage_payer
 end
