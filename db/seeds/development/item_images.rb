7720f54a6b2d7d63d7ff79e0852c6911f8b11946
pixar2390 <pixar_duffy@icloud.com>
2020-06-30 12:19:27 +0900

    Update item_images.rb

--- db/seeds/development/item_images.rb
+++ db/seeds/development/item_images.rb
@@ -1,7 +1,11 @@
 6.upto(13) do |idx|
   ItemImage.create(
-    
     image: "material/items/a00#{idx}.jpg",
-    item_id: idx - 5,
+    item_id: idx - 5
   )
+  #ハトムギ化粧水の画像を追加
+  if idx == 6 then
+    ItemImage.create(image: "material/items/a00#{idx}-1.jpg", item_id: 1)
+    ItemImage.create(image: "material/items/a00#{idx}-2.jpg", item_id: 1)
+  end
 end
