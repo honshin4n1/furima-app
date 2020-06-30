38132b33d25cb3c53588743d06d0ac27ad3300c8
DXnabe02 <watanabe02masahide02@gmail.com>
2020-06-30 18:53:24 +0900

    create specfile(cardmodel and cardscontroller)

--- spec/factories/cards.rb
+++ spec/factories/cards.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :card do
-    
+    id          {"1"}
+    user_id     {"1"}
+    customer_id {"aaaaaaaaaaa"}
+    card_id     {"fergergergae"}
+    created_at  {"2020-06-30 00:00:00"}
+    updated_at  {"2020-06-30 01:00:00"}
   end
+  
 end
--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
-RSpec.describe Card, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Card do
+  describe '#pay' do
+    it 'card情報が存在するとき登録できる' do
+      user = create(:user)
+      card = build(:card)
+      expect(card).to be_valid
+    end
+
+    it 'customer_idが存在しないとき登録できない' do
+      user = create(:user)
+      card = build(:card, customer_id: "")
+      card.valid?
+      expect(card.errors[:customer_id]).to include("can't be blank")
+    end
+
+    it 'card_idが存在しないとき登録できない' do
+      user = create(:user)
+      card = build(:card, card_id: "")
+      card.valid?
+      expect(card.errors[:card_id]).to include("can't be blank")
+    end
+
+  end
 end
