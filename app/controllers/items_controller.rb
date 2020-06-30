cc3da699a3861893c9342f947f002f489b62100c
pixar2390 <pixar_duffy@icloud.com>
2020-06-30 18:00:16 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,7 +5,7 @@ class ItemsController < ApplicationController
     # N＋１問題を解消するためにincludesメソッドを使ってimte_imagesテーブルの情報をインスタンス変数に格納
     @products = Item.where(deal_state: 0).includes(:item_images).order(created_at: :DESC)
   end
-  
+
   def new
     # レイヤーを変更
     render layout: "nothing"
@@ -16,7 +16,8 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.new
+    # item一個に対してはitem_imageのSQLは一つしか走らないのでincludeは不要
+    @item = Item.find(params[:id])
   end
 
   def purchase
