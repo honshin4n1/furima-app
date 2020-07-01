253abe48bee6855a250cda1b6d44720ceb1aac81
pixar2390 <pixar_duffy@icloud.com>
2020-07-01 18:58:27 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,6 +8,13 @@ class ItemsController < ApplicationController
 
   def new
     @category = Category.all
+    @item = Item.new
+    @item.item_images.new
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
     # レイヤーを変更
     render layout: "nothing"
   end
@@ -30,6 +37,24 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
     @name = @item.name
     @item.destroy
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name,
+                                    :price,
+                                    :introduction,
+                                    :size,
+                                    :condition,
+                                    :deal_state,
+                                    :user_id,
+                                    :brand_id,
+                                    :category_id,
+                                    :prefecture_id,
+                                    :preparation_day_id,
+                                    :postage_payer_id,
+                                    item_images_attributes: [:image])
   end
 
   def purchase
