e9a2bd13ee77f899f5d73fe57b0dcd64e465f559
honshin4n1 <million_films.sh@icloud.com>
2020-07-01 16:02:32 +0900

    create factories users profiles sending_destinations

--- spec/factories/sending_destinations.rb
+++ spec/factories/sending_destinations.rb
@@ -1,5 +1,15 @@
 FactoryBot.define do
   factory :sending_destination do
-    
+    first_name          {"花子"}
+    family_name         {"鈴木"}
+    first_name_kana     {"ハナコ"}
+    family_name_kana    {"スズキ"}
+    post_code           {"810-0000"}
+    prefecture          {"福岡県"}
+    city                {"福岡市中央区"}
+    house_number        {"中洲1-1-1"}
+    building_name       {"ビル8F"}
+    phone_number        {"09000000000"}
+    association :user
   end
 end
