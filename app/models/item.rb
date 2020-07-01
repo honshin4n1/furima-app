a1daa0c8f622fbe6d8c8616f29da1e60e6eea1d9
pixar2390 <pixar_duffy@icloud.com>
2020-07-01 18:58:19 +0900

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,6 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  accepts_nested_attributes_for :item_images
-  has_many :item_images, dependent: :destroy
+  has_many :item_images
   has_many :comments, dependent: :destroy
   has_many :evaluations
   has_many :favorites
@@ -11,4 +10,6 @@ class Item < ApplicationRecord
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :preparation_day
   belongs_to_active_hash :postage_payer
+  accepts_nested_attributes_for :item_images
+  accepts_nested_attributes_for :item_images, allow_destroy: true
 end
