f78e796f1778e9755be0820f66eb68eef5abc2c9
pixar2390 <pixar_duffy@icloud.com>
2020-07-01 17:18:54 +0900

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,8 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  has_many :comments, dependent: :destroy
+  accepts_nested_attributes_for :item_images
   has_many :item_images, dependent: :destroy
+  has_many :comments, dependent: :destroy
   has_many :evaluations
   has_many :favorites
   belongs_to :category
