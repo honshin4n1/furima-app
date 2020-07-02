8b3fe14e9a7a8c76ee9c5be13f6f21aa1f333209
pixar2390 <pixar_duffy@icloud.com>
2020-07-02 17:30:59 +0900

    Update item_images.rb

--- db/seeds/development/item_images.rb
+++ db/seeds/development/item_images.rb
@@ -1,13 +1,15 @@
-6.upto(13) do |idx|
-  ItemImage.create(
-    image: "material/items/a00#{idx}.jpg",
-    item_id: idx - 5
-  )
-  #ハトムギ化粧水の画像を追加
-  if idx == 6 then
-    ItemImage.create(image: "material/items/a00#{idx}-1.jpg", item_id: 1)
-    ItemImage.create(image: "material/items/a00#{idx}-2.jpg", item_id: 1)
-  end
-end
+#items.rbで画像も一緒に保存
+# 6.upto(13) do |idx|
+#   dummy = Item.new
+#   ItemImage.create(
+#     image: "material/items/a00#{idx}.jpg",
+#     item_id: idx - 5
+#   )
+#   #ハトムギ化粧水の画像を追加
+#   if idx == 6 then
+#     ItemImage.create(image: "material/items/a00#{idx}-1.jpg", item_id: 1)
+#     ItemImage.create(image: "material/items/a00#{idx}-2.jpg", item_id: 1)
+#   end
+# end
 
-# item_images_attributes.create(image: "material/items/a006-1.jpg", item_id: 1)
+
