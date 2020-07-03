f8c153ffbd023b0280e690338ccee306a17c019e
honshin4n1 <million_films.sh@icloud.com>
2020-07-03 16:51:09 +0900

    add validates models item item_image

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,10 +5,13 @@ class Item < ApplicationRecord
   has_many :evaluations
   has_many :favorites
   belongs_to :category
-  belongs_to :brand
+  belongs_to :brand, optional: true
   belongs_to :user
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :preparation_day
   belongs_to_active_hash :postage_payer
   accepts_nested_attributes_for :item_images, allow_destroy: true
+
+  validates :name, :introduction, :price, :size, :condition, :deal_state,
+            :prefecture_id, :preparation_day_id, :postage_payer_id, presence: true
 end
