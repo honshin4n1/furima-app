fc5758a9edf99d3773ceb046f7edb2823395d8a6
pixar2390 <pixar_duffy@icloud.com>
2020-07-05 15:33:36 +0900

    Update sending_destinations.rb

--- db/seeds/development/sending_destinations.rb
+++ db/seeds/development/sending_destinations.rb
@@ -5,16 +5,16 @@ gnames_kana = %w(タロウ ジロウ ハナコ ジョン マイク)
 phone_numbers =%w(080-1648-9462 080-6382-8423 080-7123-3905 090-5722-8873 080-9226-5462)
 0.upto(fnames.length - 1) do |idx|
   SendingDestination.create(
-    first_name:fnames[idx]
-    family_name:gnames[idx]
-    first_name_kana:fnames_kana[idx]
-    family_name_kana:gnames_kana[idx]
-    post_code:"839-5578"
-    prefecture:rand(1..47)
-    city:"八代市"
-    house_number:"467-58"
-    building_name:"アンソレイユ206号室"
-    phone_number:phone_numbers[idx]
+    first_name:fnames[idx],
+    family_name:gnames[idx],
+    first_name_kana:fnames_kana[idx],
+    family_name_kana:gnames_kana[idx],
+    post_code:"839-5578",
+    prefecture:rand(1..47),
+    city:"八代市",
+    house_number:"467-58",
+    building_name:"アンソレイユ206号室",
+    phone_number:phone_numbers[idx],
     user_id: idx
   )
 end
