2f3e616c81cf11f30144e54acb0b19e83a3ae04c
pixar2390 <pixar_duffy@icloud.com>
2020-07-05 15:26:11 +0900

    Update sending_destinations.rb

--- db/seeds/development/sending_destinations.rb
+++ db/seeds/development/sending_destinations.rb
@@ -1,14 +1,23 @@
-names = %w(Taro Jiro Hana John Mike Sophy Bill Alex Mary Tom)
-fnames = ["佐藤", "鈴木", "高橋", "田中"]
-gnames = ["太郎", "次郎", "花子"]
-0.upto(9) do |idx|
-  Member.create(
-    number: idx + 10,
-    name: names[idx],
-    full_name: "#{fnames[idx % 4]} #{gnames[idx % 3]}",
-    email: "#{names[idx]}@example.com",
-    birthday: "1981-12-01",
-    sex: [1, 1, 2][idx % 3],
-    administrator: (idx == 0)
+fnames = %w(佐藤 鈴木 高橋 Axelrod Garland)
+gnames = %w(太郎 次郎 花子 John Mike)
+fnames_kana = %w(サトウ スズキ タカハシ アクセルロッド ガーランド)
+gnames_kana = %w(タロウ ジロウ ハナコ ジョン マイク)
+phone_numbers =%w(080-1648-9462 080-6382-8423 080-7123-3905 090-5722-8873 080-9226-5462)
+0.upto(fnames.length - 1) do |idx|
+  SendingDestination.create(
+    first_name:fnames[idx]
+    family_name:gnames[idx]
+    first_name_kana:fnames_kana[idx]
+    family_name_kana:gnames_kana[idx]
+    post_code:"839-5578"
+    prefecture:rand(1..47)
+    city:"八代市"
+    house_number:"467-58"
+    building_name:"アンソレイユ206号室"
+    phone_number:phone_numbers[idx]
+    user_id: idx
   )
 end
+
+
+
